6baa76af658da4f9f03bd7a997bb183c23b492e3
Jesse Zhao <jessezhao1992@gmail.com>
2021-06-21 14:08:11 -0700

    Uses gp-saml-gui for epic vpn

--- tools/epicgames/vpn.sh
+++ tools/epicgames/vpn.sh
@@ -1,24 +1,24 @@
 # !/bin/bash
-# ESC="$(printf '\033')"
-# NORMAL="${ESC}[0m"
-# YELLOW="${ESC}[1;33m"
-# CYAN="${ESC}[36m"
-# box()
-# {
-#   # "c 1 c" is to add 4 chars to t length.
-#   t="c $1 c"
-#   # c is $2 or '='
-#   c=${2:-=}
-#   # substitute all chars in t with $c
-#   border=${YELLOW}${t//?/$c}${NORMAL}
-#   c=${YELLOW}$c${NORMAL}
-#   echo $border
-#   echo "$c ${CYAN}$1${NORMAL} $c"
-#   echo $border
-# }
+ESC="$(printf '\033')"
+NORMAL="${ESC}[0m"
+YELLOW="${ESC}[1;33m"
+CYAN="${ESC}[36m"
+box()
+{
+	# "c 1 c" is to add 4 chars to t length.
+	t="c $1 c"
+	# c is $2 or '='
+	c=${2:-=}
+	# substitute all chars in t with $c
+	border=${YELLOW}${t//?/$c}${NORMAL}
+	c=${YELLOW}$c${NORMAL}
+	echo $border
+	echo "$c ${CYAN}$1${NORMAL} $c"
+	echo $border
+}
 
 GATEWAY="vpn-se2.it.epicgames.com"
-echo "Initiating VPN pre-logon to gateway: $GATEWAY"
+box "Initiating VPN pre-logon to gateway: $GATEWAY"
 eval $(gp-saml-gui --gateway $GATEWAY --clientos=Windows)
-echo "Pre-logon successful | Cookie: $COOKIE"
-eval $(echo $COOKIE | sudo openconnect --protocol=gp --user=$USER --os=win --usergroup=gateway:prelogin-cookie --passwd-on-stdin $GATEWAY)
+box "Pre-logon successful | Cookie: $COOKIE"
+eval $(echo $COOKIE | sudo openconnect --protocol=gp --user="$USER" --os="$OS" --usergroup=gateway:prelogin-cookie --passwd-on-stdin $GATEWAY)
